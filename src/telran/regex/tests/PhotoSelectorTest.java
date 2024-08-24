4b8f87d8a9e20ed488a006f515d41b70dd2122e2
Vladimir <vladimirrogalev1@gmail.com>
2024-08-24 18:12:48 +0300

    added method check name pictures and tests

--- src/telran/regex/tests/PhotoSelectorTest.java
+++ src/telran/regex/tests/PhotoSelectorTest.java
@@ -51,9 +51,88 @@ class PhotoSelectorTest {
 			"London\\20151001_231035.jpg",	
 			};
 		
-		assertEquals(expected, actual);
-		
-		
+		assertArrayEquals(expected, actual);
+	}
+	
+	@Test
+	
+	void testAllAutumnPictures() {
+		String regex = "\\d{4}(09|10|11)\\d{2}";
+		String [] actual = PhotoSelector.selectPictures(pictures, regex);
+		String [] expected = {
+				"London\\20151001_110023.jpg",	
+				"London\\20151001_121705.jpg",	
+				"London\\20151001_231035.jpg",	
+				"Chichago\\20151111_232000.png",
+				
+		};
+		assertArrayEquals(expected, actual);
+	}
+	
+	@Test
+	void testAllSpring2015Pictures() {
+		String regex = "2015(03|04|05)\\d{2}";
+		String [] actual = PhotoSelector.selectPictures(pictures, regex);
+		String [] expected = {
+				"Paris\\20150301_211035.jpg",	
+				"Paris\\20150401_110023.gif",	
+				"Paris\\20150401_181705.jpg",	
+				"Paris\\20150501_231035.gif",
+				"Chichago\\20150301_120001.png",
+
+				
+		};
+		assertArrayEquals(expected, actual);
+	}
+	@Test
+	void testAllNightPictures() {
+		String regex ="[_](1[8-9]|2[0-4])\\d+";
+		String [] actual = PhotoSelector.selectPictures(pictures, regex);
+		String [] expected = {
+				"Paris\\20150301_211035.jpg",	
+				"Paris\\20150401_181705.jpg",	
+				"Paris\\20150501_231035.gif",
+				"London\\20140601_211035.gif",
+				"London\\20151001_231035.jpg",
+				"Chichago\\20151111_232000.png",
+				
+		};
+		assertArrayEquals(expected, actual);
+	}
+
+	@Test
+	void testAllNightPicturesFromChicago() {
+		String regex ="^(Chichago).*[_](1[8-9]|2[0-4])\\d+";
+		String [] actual = PhotoSelector.selectPictures(pictures, regex);
+		String [] expected = {
+				"Chichago\\20151111_232000.png",
+				
+		};
+
+		assertArrayEquals(expected, actual);
+
+	}
+	@Test
+	void testAllJpgAndPngs() {
+		String regex ="(jpg|png)";
+		String [] actual = PhotoSelector.selectPictures(pictures, regex);
+		String [] expected = {
+				"Paris\\20140101_090000.jpg",
+				"Paris\\20140201_121005.jpg",
+				"Paris\\20150301_211035.jpg",	
+				"Paris\\20150401_181705.jpg",	
+				"London\\20140205_090000.jpg",	
+				"London\\20140205_121005.jpg",	
+				"London\\20151001_110023.jpg",	
+				"London\\20151001_121705.jpg",	
+				"London\\20151001_231035.jpg",	
+				"Chichago\\20150301_120001.png",
+				"Chichago\\20151111_232000.png",
+				
+		};
+
+		assertArrayEquals(expected, actual);
+
 	}
 
 }
--- src/telran/regex/tools/PhotoSelector.java
+++ src/telran/regex/tools/PhotoSelector.java
@@ -1,9 +1,25 @@
 package telran.regex.tools;
 
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PhotoSelector {
-	public static String[] selectPictures (String[] picture, String regex) {
+	public static String[] selectPictures(String[] picture, String regex) {
 	
-		return null;
+		 Pattern pattern = Pattern.compile(regex);
+	        Matcher matcher;
+	        StringBuilder builder = new StringBuilder();
+	        for (int i = 0; i < picture.length; i++) {
+	            matcher = pattern.matcher(picture[i]);
+	            if (matcher.find()) {
+	                builder.append(picture[i]);
+	                builder.append(",");
+	                
+	            }
+	        }
+	        String[] selectedPictures = builder.toString().split(",");
+	        return selectedPictures;
 	}
 
 }
